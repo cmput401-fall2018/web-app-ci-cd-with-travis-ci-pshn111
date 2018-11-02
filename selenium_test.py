e1f43361465b04f16c547f7f5c4feacedef1e818
pshn111 <spei@ualberta.ca>
2018-11-02 16:24:36 -0600

    test

--- selenium_test.py
+++ selenium_test.py
@@ -3,13 +3,12 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
 def test_home():
-	# options = Options()
-	# options.add_argument("--no-sandbox")# Bypass OS security model
-	# options.add_argument("--headless")# Runs Chrome in headless mode.
-	# driver = webdriver.Chrome(chrome_options=options)
-    driver = webdriver.Chrome()
-    
+    options = Options()
+    options.add_argument("--no-sandbox")# Bypass OS security model
+    options.add_argument("--headless")# Runs Chrome in headless mode.
+    driver = webdriver.Chrome(chrome_options=options, executable_path='chromedriver.exe')
     driver.get("http://162.246.157.125:8000/")
+
     assert driver.find_element_by_id("name") != None
     assert driver.find_element_by_id("about") != None
     assert driver.find_element_by_id("education") != None
