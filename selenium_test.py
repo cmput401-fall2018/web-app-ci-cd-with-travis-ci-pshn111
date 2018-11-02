d8570adf636cd551c82f073ea59362579826b210
pshn111 <spei@ualberta.ca>
2018-11-02 16:00:04 -0600

    test

--- selenium_test.py
+++ selenium_test.py
@@ -3,18 +3,21 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
 def test_home():
-	options = Options()
-	options.add_argument("--no-sandbox")# Bypass OS security model
-	options.add_argument("--headless")# Runs Chrome in headless mode.
-	driver = webdriver.Chrome(chrome_options=options)
-	driver.get("http://162.246.157.125:8000/")
-	
+	# options = Options()
+	# options.add_argument("--no-sandbox")# Bypass OS security model
+	# options.add_argument("--headless")# Runs Chrome in headless mode.
+	# driver = webdriver.Chrome(chrome_options=options)
+    driver = webdriver.Chrome()
     
-	assert driver.find_element_by_id("name") != None
-	assert driver.find_element_by_id("about") != None
-	assert driver.find_element_by_id("education") != None
-	assert driver.find_element_by_id("skills") != None
-	assert driver.find_element_by_id("work") != None
-	assert driver.find_element_by_id("contact") != None
+    driver.get("http://162.246.157.125:8000/")
+    assert driver.find_element_by_id("name") != None
+    assert driver.find_element_by_id("about") != None
+    assert driver.find_element_by_id("education") != None
+    assert driver.find_element_by_id("skills") != None
+    assert driver.find_element_by_id("work") != None
+    assert driver.find_element_by_id("contact") != None     
 
 test_home()
+
+
+
