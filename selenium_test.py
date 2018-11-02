fe52df3c969149d6ff4b13f8564b5d4811c1ede7
pshn111 <spei@ualberta.ca>
2018-11-02 16:56:09 -0600

    test

--- selenium_test.py
+++ selenium_test.py
@@ -6,7 +6,7 @@ def test_home():
     options = Options()
     options.add_argument("--no-sandbox")# Bypass OS security model
     options.add_argument("--headless")# Runs Chrome in headless mode.
-    driver = webdriver.Chrome(chrome_options=options, executable_path='chromedriver.exe')
+    driver = webdriver.Chrome(chrome_options=options)
     driver.get("http://162.246.157.125:8000/")
 
     assert driver.find_element_by_id("name") != None
